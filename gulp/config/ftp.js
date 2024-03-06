6aa3cc2b4e6938b79db0d5df011ee41427dcf5ad
gorsun4ic <gorsunfoster@ukr.net>
2024-03-06 10:08:30 +0200

    Initial commit

---
+++ gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let configFTP = {
+    host: "t124319.hostde16.fornex.org", // FTP servers address
+    user: "t124319", // Username
+    password: "if6f5zUo*dQpC@4#", // Password
+    parallel: 5 
+}
